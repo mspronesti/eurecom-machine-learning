b36cb3b51e425b1bfaa01e450ea6dbed594be3e8
mariazuluaga <maria.zuluaga@gmail.com>
2020-10-16 15:47:34 +0200

    Lab 1: Oct 20 2020

--- labs/lab1/utils.py
+++ labs/lab1/utils.py
@@ -1,53 +1,40 @@
 import numpy as np
-from numpy import linalg
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from sklearn.mixture import GaussianMixture
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
-def split_data(df,label):
+def gaussians():
     '''
-    utilitary function used to generate data splits for the first two parts of the lab
+    Generates data from 3 different (in mean) 2D multivariate Gaussian distributions.
+    Means, covariances and number of samples are fixed.
     '''
-    try:
-    # Doesn't work: a value is missing
-        train_data, test_data = train_test_split(df, test_size = 0.2, 
-                                                 stratify=df[label])
-    except:
-        # Count the missing lines and drop them
-        missing_rows = np.isnan(df[label])
-        #print("Uh oh, {} lines missing data! Dropping them".format(np.sum(missing_rows)))
-        df = df.dropna(subset=[label])
-        train_data, test_data = train_test_split(df, test_size = 0.2, 
-                                                 stratify=df[label])
-        
-    return train_data, test_data
-
-
-
-def fit_model(X, y):
     '''
-    Least squares solution of a linear model of the form y = W^Tx
-    returns the estimated weights vector
+	INPUTS : /
+	'''
     '''
-    X_t = np.transpose(X) #X^T
-    X_t_X = X_t.dot(X)    #X^TX
-    X_T_y = X_t.dot(y)    #X^Ty
+	OUTPUTS:
+	- 'points' : numpy array in 2D containing the x-axis and y-axis coordinates of N points
+	- 'y' : numpy array in 1D containing the labels of each one of the N points in 'points'
+	'''
     
-    #An alternative and more efficient way to compute: using a linear solver to solve the eq Ax = b
-    w = linalg.solve(X_t_X, X_T_y)
-    return w
-
-
-def fit_logreg(X, y):
-    '''
-    Wraps initialization and training of Logistic regression
-    '''
-    logreg = LogisticRegression(C=1e20, solver='liblinear', max_iter=200) #
-    logreg.fit(X, y)
+    N=50
+    means = np.array([[4.5, 4.5],
+                      [5.5, 2.5],
+                      [6.3,3.5]])
+    covs = np.array([np.diag([0.5, 0.5]),
+                     np.diag([0.5, 0.5]),
+                     np.diag([0.5, 0.5])])
+    y=[]
+    points = []
+    for i in range(len(means)):
+        x = np.random.multivariate_normal(means[i], covs[i], N )
+        points.append(x)
+        y.append(i*np.ones(N)) 
+    points = np.concatenate(points)
+    y=np.concatenate(y)
     
-    return logreg
+    return points, y
+
 
 def comparing_plots(xx,yy, X, y, data_1, data_2, title_1, title_2):
     '''
@@ -55,6 +42,21 @@ def comparing_plots(xx,yy, X, y, data_1, data_2, title_1, title_2):
     It displays the training data with different colours and uses the same colours to differentiate 
     the different regions defined by the decision boundaries.
     '''
+    '''
+    INPUTS :
+    - xx : x-axis coordinates of input testing points
+    - yy : y-axis coordinates of input testing points
+    - X : set of coordinates of the input training points
+    - y : set of labels of the training points
+    - data1 : set of predicted labels using 'title_1' (e.g. scikit-learn or your implementation or logistic regression or ...)
+    - data2 : set of predicted labels using 'title_2'
+    - title1 : character string specifying how 'data1' are obtained
+    - title2 : character string specifying how 'data2' are obtained
+	'''
+    '''
+	OUTPUTS: /
+    '''
+    
     cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
     cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
@@ -78,26 +80,3 @@ def comparing_plots(xx,yy, X, y, data_1, data_2, title_1, title_2):
     plt.ylim(yy.min(), yy.max())
     plt.title(title_2)
     plt.show()
-    
-def gaussians():
-    '''
-    Generates data from a multivariate Gaussian distribution.
-    Means, covariances and number of samples are fixed.
-    '''
-    N=50
-    means = np.array([[4.5, 4.5],
-                      [5.5, 2.5],
-                      [6.3,3.5]])
-    covs = np.array([np.diag([0.5, 0.5]),
-                     np.diag([0.5, 0.5]),
-                     np.diag([0.5, 0.5])])
-    y=[]
-    points = []
-    for i in range(len(means)):
-        x = np.random.multivariate_normal(means[i], covs[i], N )
-        points.append(x)
-        y.append(i*np.ones(N)) 
-    points = np.concatenate(points)
-    y=np.concatenate(y)
-    
-    return points, y
